1833b59100c39e8774ca7cb856fb848e29c19e01
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2015-12-25 07:16:08 +0900

    認証情報を変更

--- ppa/p41authopt.php
+++ ppa/p41authopt.php
@@ -2,9 +2,9 @@
 //!	認証処理サンプル	認証処理用オプションデータ
 $ppopts = array('db_host'		=> 'localhost',
 				'db_user'		=> 'ppguest',
-				'db_password'	=> 'GGGGGGGGGG',
+				'db_password'	=> '1234',
 				'db_name'		=> 'ppdb',
-				'security_salt'	=> 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+				'security_salt'	=> 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMN',
 				'login_page'	=> '/ppa/p41login.php',
 				'loginok_page'	=> '/ppa/p41auth1.php',
 				'sessname'		=> 'SESSCAMPAIGN'	);
